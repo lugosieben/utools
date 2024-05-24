c4613e6c843ed6e1d0c629700dc891038e077a34
lugosieben <62535192+lugosieben@users.noreply.github.com>
2024-05-24 15:31:32 +0200

    Initial commit

---
+++ src/main/java/net/lugo/utools/UToolsClient.java
@@ -0,0 +1,10 @@
+package net.lugo.utools;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class UToolsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
