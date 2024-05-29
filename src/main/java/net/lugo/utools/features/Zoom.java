7dcbdbd5b7eea877914f41969f2d0b1247f095c6
lugosieben <62535192+lugosieben@users.noreply.github.com>
2024-05-29 14:39:33 +0200

    Improve Zoom (Allow "invalid" values, smooth camera)

--- src/main/java/net/lugo/utools/features/Zoom.java
+++ src/main/java/net/lugo/utools/features/Zoom.java
@@ -1,27 +1,17 @@
 package net.lugo.utools.features;
 
-import me.shedaniel.autoconfig.AutoConfig;
-import net.lugo.utools.config.ModConfig;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.option.SimpleOption;
-
 public class Zoom {
-    private static final SimpleOption<Integer> fov = MinecraftClient.getInstance().options.getFov();
-    private static final ModConfig config = AutoConfig.getConfigHolder(ModConfig.class).getConfig();
-    private static int originalFov = fov.getValue();
-
-    public static void zoom (long multiplier) {
-        int newvalue = (int)((long) fov.getValue() / multiplier );
-        fov.setValue(newvalue);
-    }
+    private static boolean zooming = false;
 
     public static void on () {
-        originalFov = fov.getValue();
-        if (config.zoomMultiplier == 0) return;
-        zoom(config.zoomMultiplier);
+        zooming = true;
     }
 
     public static void off() {
-        fov.setValue(originalFov);
+        zooming = false;
+    }
+
+    public static boolean isZooming () {
+        return zooming;
     }
 }
