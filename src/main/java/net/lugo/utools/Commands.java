39f15c3e66372650883a68d06b7d8e76e241962a
lugosieben <62535192+lugosieben@users.noreply.github.com>
2024-05-29 12:25:11 +0200

    Add UTools Commands - Add /utools config command

--- src/main/java/net/lugo/utools/Commands.java
+++ src/main/java/net/lugo/utools/Commands.java
@@ -2,9 +2,12 @@ package net.lugo.utools;
 
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.lugo.utools.command.FullBrightCommand;
+import net.lugo.utools.command.UToolsCommand;
+import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
 
 public class Commands {
     public static void registerCommands() {
         CommandRegistrationCallback.EVENT.register(FullBrightCommand::register);
+        CommandRegistrationCallback.EVENT.register(UToolsCommand::register);
     }
 }
---
+++ src/main/java/net/lugo/utools/command/UToolsCommand.java
@@ -0,0 +1,26 @@
+package net.lugo.utools.command;
+
+import com.mojang.brigadier.CommandDispatcher;
+import me.shedaniel.autoconfig.AutoConfig;
+import net.lugo.utools.config.ModConfig;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.command.CommandRegistryAccess;
+import net.minecraft.server.command.CommandManager;
+import net.minecraft.server.command.ServerCommandSource;
+
+public class UToolsCommand {
+    final static MinecraftClient MC = MinecraftClient.getInstance();
+
+    public static void register(CommandDispatcher<ServerCommandSource> dispatcher, CommandRegistryAccess commandRegistryAccess, CommandManager.RegistrationEnvironment registrationEnvironment) {
+        dispatcher.register(CommandManager.literal("utools")
+                .then(CommandManager.literal("config")
+                        .executes(context -> {
+                            MC.execute(() -> {
+                                MC.setScreen(AutoConfig.getConfigScreen(ModConfig.class, MC.currentScreen).get());
+                            });
+                            return 1;
+                        })
+                )
+        );
+    }
+}
