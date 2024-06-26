9203e80bb3cc8e2e00614062e3fb06cb2a95f0dd
lugosieben <62535192+lugosieben@users.noreply.github.com>
2024-06-26 19:52:16 +0200

    Clean up config with categories

--- src/main/java/net/lugo/utools/config/ModConfig.java
+++ src/main/java/net/lugo/utools/config/ModConfig.java
@@ -9,14 +9,18 @@ import net.lugo.utools.features.ClientTime.ClientTimeType;
 @Config(name = UTools.MOD_ID)
 public class ModConfig  implements ConfigData {
 
+    @ConfigEntry.Category("Fullbright")
     @ConfigEntry.BoundedDiscrete(min = 0,max = 1500)
     public int normalGammaPercentage = 100;
 
+    @ConfigEntry.Category("Fullbright")
     @ConfigEntry.BoundedDiscrete(min = 0,max = 1500)
     public int fullGammaPercentage = 1500;
 
+    @ConfigEntry.Category("Zoom")
     public int zoomMultiplier = 3;
 
+    @ConfigEntry.Category("Zoom")
     @ConfigEntry.BoundedDiscrete(min = 0, max = 1)
     @ConfigEntry.Gui.Tooltip(count = 3)
     public double zoomSpeed = 0.1;
@@ -24,7 +28,9 @@ public class ModConfig  implements ConfigData {
     @ConfigEntry.BoundedDiscrete(min=1,max = 15)
     public int lightOverlayThreshold = 1;
 
+    @ConfigEntry.Category("ClientTimeWeather")
     public boolean clientWeatherClear = false;
+    @ConfigEntry.Category("ClientTimeWeather")
     public ClientTimeType clientTimeType = ClientTimeType.Disabled;
     
     @ConfigEntry.Gui.Tooltip()
