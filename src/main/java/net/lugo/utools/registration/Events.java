e2c65a07ae2b03c4d39e2274e284e121e54f9449
lugosieben <62535192+lugosieben@users.noreply.github.com>
2024-06-29 11:34:13 +0200

    Add autoattack feature

--- src/main/java/net/lugo/utools/registration/Events.java
+++ src/main/java/net/lugo/utools/registration/Events.java
@@ -1,10 +1,13 @@
 package net.lugo.utools.registration;
 
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderEvents;
+import net.lugo.utools.features.AutoAttack;
 import net.lugo.utools.features.LightOverlay;
 
 public class Events {
     public static void registerEvents() {
         WorldRenderEvents.END.register(LightOverlay::renderEnd);
+        ClientTickEvents.END_CLIENT_TICK.register(AutoAttack::clientTickEvent);
     }
 }
