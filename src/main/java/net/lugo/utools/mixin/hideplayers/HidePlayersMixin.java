141ea560d828c30b30a9f9a9ccf65135fd89efb9
lugosieben <62535192+lugosieben@users.noreply.github.com>
2024-11-16 16:56:17 +0100

    Fix HidePlayers and NoPumpkinOverlay for 1.21.3+

--- src/main/java/net/lugo/utools/mixin/hideplayers/HidePlayersMixin.java
+++ src/main/java/net/lugo/utools/mixin/hideplayers/HidePlayersMixin.java
@@ -1,28 +1,19 @@
 package net.lugo.utools.mixin.hideplayers;
 
-import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import net.lugo.utools.features.HidePlayers;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.network.AbstractClientPlayerEntity;
-import net.minecraft.client.render.VertexConsumerProvider;
-import net.minecraft.client.render.entity.PlayerEntityRenderer;
-import net.minecraft.client.render.entity.state.PlayerEntityRenderState;
-import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.client.render.Frustum;
+import net.minecraft.client.render.entity.EntityRenderer;
+import net.minecraft.entity.Entity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(PlayerEntityRenderer.class)
+@Mixin(EntityRenderer.class)
 public class HidePlayersMixin {
-    // TODO: Implement this for 1.21.3+
-    /*
-    @ModifyReturnValue(method = "shouldRenderFeatures(Lnet/minecraft/client/render/entity/state/PlayerEntityRenderState;)Z", at = @At("HEAD"))
-    private void shouldRenderFeatures(PlayerEntityRenderState playerEntityRenderState, CallbackInfoReturnable<Boolean> cir) {
-        if (HidePlayers.get() && MinecraftClient.getInstance().player != null) {
-            if (MinecraftClient.getInstance().player.getName() != playerEntityRenderState.playerName) cir.setReturnValue(false);
-        }
+    @Inject(method = "shouldRender", at = @At("RETURN"), cancellable = true)
+    private void shouldRender(Entity entity, Frustum frustum, double x, double y, double z, CallbackInfoReturnable<Boolean> cir) {
+        if (HidePlayers.get() && entity.isPlayer() && entity != MinecraftClient.getInstance().player) cir.setReturnValue(false);
     }
-    */
 }
--- src/main/java/net/lugo/utools/mixin/visualmods/NoPumpkinOverlayMixin.java
+++ src/main/java/net/lugo/utools/mixin/visualmods/NoPumpkinOverlayMixin.java
@@ -3,8 +3,7 @@ package net.lugo.utools.mixin.visualmods;
 import net.lugo.utools.UTools;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.InGameHud;
-import net.minecraft.util.Identifier;
-import org.spongepowered.asm.mixin.Final;
+import net.minecraft.client.render.RenderTickCounter;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -12,11 +11,15 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(InGameHud.class)
 public class NoPumpkinOverlayMixin {
-    @Final private static Identifier PUMPKIN_BLUR;
 
-    @Inject(method = "renderOverlay", at = @At("HEAD"), cancellable = true)
-    private void renderOverlay (DrawContext context, Identifier texture, float opacity, CallbackInfo ci) {
-        // TODO: Implement for 1.21.3+
-        //if (texture == PUMPKIN_BLUR && UTools.getConfig().hidePumpkinOverlay) ci.cancel();
+    @Inject(
+        method = "renderMiscOverlays",
+        at = @At(value = "INVOKE",
+            target = "Lnet/minecraft/client/gui/hud/InGameHud;renderOverlay(Lnet/minecraft/client/gui/DrawContext;Lnet/minecraft/util/Identifier;F)V",
+            shift = At.Shift.BY),
+        cancellable = true
+    )
+    private void renderOverlay (DrawContext context, RenderTickCounter tickCounter, CallbackInfo ci) {
+        if (UTools.getConfig().hidePumpkinOverlay) ci.cancel();
     }
 }
