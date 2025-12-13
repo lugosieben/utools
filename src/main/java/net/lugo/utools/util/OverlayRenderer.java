ba07f3991496c42da04acedfe059d560aefbe871
lugosieben <62535192+lugosieben@users.noreply.github.com>
2025-12-13 18:48:42 +0100

    update to 1.21.11

--- src/main/java/net/lugo/utools/util/OverlayRenderer.java
+++ src/main/java/net/lugo/utools/util/OverlayRenderer.java
@@ -16,24 +16,24 @@ import net.minecraft.util.math.Vec3d;
 import org.joml.Matrix4f;
 
 public class OverlayRenderer {
-    private static final RenderPipeline LIGHT_OVERLAY_PIPELINE = RenderPipelines.register(
-        RenderPipeline.builder(RenderPipelines.POSITION_TEX_COLOR_SNIPPET)
-            .withLocation(Identifier.of(UTools.MOD_ID, "pipeline/light_overlay"))
-            .withCull(true)
-            .withDepthTestFunction(DepthTestFunction.LEQUAL_DEPTH_TEST)
-            .withDepthWrite(true)
-            .build()
-    );
-
-    private static final RenderLayer LIGHT_OVERLAY_RENDERLAYER = RenderLayer.of(
-        "utools/light_overlay",
-        1024,
-        false,
-        true,
-        LIGHT_OVERLAY_PIPELINE,
-        RenderLayer.MultiPhaseParameters.builder()
-            .build(false)
-    );
+    //private static final RenderPipeline LIGHT_OVERLAY_PIPELINE = RenderPipelines.register(
+    //    RenderPipeline.builder(RenderPipelines.POSITION_TEX_COLOR_SNIPPET)
+    //        .withLocation(Identifier.of(UTools.MOD_ID, "pipeline/light_overlay"))
+    //        .withCull(true)
+    //        .withDepthTestFunction(DepthTestFunction.LEQUAL_DEPTH_TEST)
+    //        .withDepthWrite(true)
+    //        .build()
+    //);
+//
+    //private static final RenderLayer LIGHT_OVERLAY_RENDERLAYER = RenderLayer.of(
+    //    "utools/light_overlay",
+    //    1024,
+    //    false,
+    //    true,
+    //    LIGHT_OVERLAY_PIPELINE,
+    //    RenderLayer.MultiPhaseParameters.builder()
+    //        .build(false)
+    //);
 
     private static final GpuTextureView shaderTexture = MinecraftClient.getInstance().getTextureManager().getTexture(Identifier.of(UTools.MOD_ID, "textures/cross.png")).getGlTextureView();
     private static final VertexConsumerProvider.Immediate vcp = VertexConsumerProvider.immediate(new BufferAllocator(8192));
@@ -42,41 +42,41 @@ public class OverlayRenderer {
     private static boolean batchStarted = false;
 
     public static void startBatch() {
-        if (batchStarted) return;
-
-        vertexConsumer = vcp.getBuffer(LIGHT_OVERLAY_RENDERLAYER);
-        RenderSystem.setShaderTexture(0, shaderTexture);
-        batchStarted = true;
+        //if (batchStarted) return;
+//
+        //vertexConsumer = vcp.getBuffer(LIGHT_OVERLAY_RENDERLAYER);
+        //RenderSystem.setShaderTexture(0, shaderTexture);
+        //batchStarted = true;
     }
 
     public static void addBlock(Camera camera, Vec3d pos, int r, int g, int b, float offsetY) {
-        if (!batchStarted) return;
-
-        Vec3d transformedPos = pos.subtract(camera.getPos());
-
-        matrixStack.push();
-        matrixStack.multiply(RotationAxis.POSITIVE_X.rotationDegrees(camera.getPitch()));
-        matrixStack.multiply(RotationAxis.POSITIVE_Y.rotationDegrees(camera.getYaw() + 180F));
-        matrixStack.translate(transformedPos.x, transformedPos.y + offsetY, transformedPos.z);
-
-        Matrix4f positionMatrix = matrixStack.peek().getPositionMatrix();
-
-        float rf = r / 255f;
-        float gf = g / 255f;
-        float bf = b / 255f;
-
-        vertexConsumer.vertex(positionMatrix, 0, 1, 0).color(rf, gf, bf, 1f).texture(0f, 0f).light(0, 0);
-        vertexConsumer.vertex(positionMatrix, 0, 1, 1).color(rf, gf, bf, 1f).texture(0f, 1f).light(0, 0);
-        vertexConsumer.vertex(positionMatrix, 1, 1, 1).color(rf, gf, bf, 1f).texture(1f, 1f).light(0, 0);
-        vertexConsumer.vertex(positionMatrix, 1, 1, 0).color(rf, gf, bf, 1f).texture(1f, 0f).light(0, 0);
-
-        matrixStack.pop();
+        //if (!batchStarted) return;
+//
+        //Vec3d transformedPos = pos.subtract(camera.getPos());
+//
+        //matrixStack.push();
+        //matrixStack.multiply(RotationAxis.POSITIVE_X.rotationDegrees(camera.getPitch()));
+        //matrixStack.multiply(RotationAxis.POSITIVE_Y.rotationDegrees(camera.getYaw() + 180F));
+        //matrixStack.translate(transformedPos.x, transformedPos.y + offsetY, transformedPos.z);
+//
+        //Matrix4f positionMatrix = matrixStack.peek().getPositionMatrix();
+//
+        //float rf = r / 255f;
+        //float gf = g / 255f;
+        //float bf = b / 255f;
+//
+        //vertexConsumer.vertex(positionMatrix, 0, 1, 0).color(rf, gf, bf, 1f).texture(0f, 0f).light(0, 0);
+        //vertexConsumer.vertex(positionMatrix, 0, 1, 1).color(rf, gf, bf, 1f).texture(0f, 1f).light(0, 0);
+        //vertexConsumer.vertex(positionMatrix, 1, 1, 1).color(rf, gf, bf, 1f).texture(1f, 1f).light(0, 0);
+        //vertexConsumer.vertex(positionMatrix, 1, 1, 0).color(rf, gf, bf, 1f).texture(1f, 0f).light(0, 0);
+//
+        //matrixStack.pop();
     }
 
     public static void endBatch() {
-        if (!batchStarted) return;
-
-        vcp.draw();
-        batchStarted = false;
+        //if (!batchStarted) return;
+//
+        //vcp.draw();
+        //batchStarted = false;
     }
 }
