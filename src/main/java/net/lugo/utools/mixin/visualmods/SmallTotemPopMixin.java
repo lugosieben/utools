a6761feeb73ae82647cec524c6424a71c682adf6
lugosieben <62535192+lugosieben@users.noreply.github.com>
2026-04-19 13:54:18 +0200

    1.8.1

--- src/main/java/net/lugo/utools/mixin/visualmods/SmallTotemPopMixin.java
+++ src/main/java/net/lugo/utools/mixin/visualmods/SmallTotemPopMixin.java
@@ -1,15 +1,19 @@
 package net.lugo.utools.mixin.visualmods;
 
+import com.mojang.blaze3d.vertex.PoseStack;
 import net.lugo.utools.config.ModConfig;
 import net.minecraft.client.renderer.ScreenEffectRenderer;
+import net.minecraft.client.renderer.SubmitNodeCollector;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ScreenEffectRenderer.class)
 public class SmallTotemPopMixin {
-    @ModifyVariable(method = "renderItemActivationAnimation", at = @At("STORE"), ordinal = 8)
-    private float renderFloatingItem(float n) {
-        return n * ((float) ModConfig.totemPopSize / 100);
+    @Inject(method = "renderItemActivationAnimation", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/vertex/PoseStack;scale(FFF)V", shift = At.Shift.AFTER))
+    private void renderItemActivationAnimation(PoseStack poseStack, float partialTicks, SubmitNodeCollector submitNodeCollector, CallbackInfo ci) {
+        float size =  (float) ModConfig.totemPopSize / 100;
+        poseStack.scale(size, size, size);
     }
 }
